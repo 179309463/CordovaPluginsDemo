e10cd152554a7b28ba2e7d642114cc29fdb47cec
sunny <kebenxiaoming@users.noreply.github.com>
2016-03-16 10:58:24 +0800

    Update customPlugin.java
    加个提示可好

--- cordova-plugin-custom/src/android/customPlugin.java
+++ cordova-plugin-custom/src/android/customPlugin.java
@@ -23,6 +23,8 @@ public class customPlugin extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         
         if (action.equals(customPlugin.METHOD_ECHO)) {
+            //加个Toast提示
+            Toast.makeText(this.cordova.getActivity(),action,Toast.LENGTH_SHORT).show();
             final String isRunBackgroud = args.getString(0);
             if(isRunBackgroud.equals("false")) {
                 callbackContext.success("Invoke success!");
